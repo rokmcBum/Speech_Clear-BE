a2643e9c9e069019c420eb15674cd48edc50f8b6
rokmcBum <peter4024@naver.com>
2025-11-22 16:38:12 +0900

    fix: add cors allow address

--- app/main.py
+++ app/main.py
@@ -1,8 +1,22 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api import voice_router, user_router, category_router
 
 app = FastAPI(title="Speech Clear")
+
+origins = [
+    "http://localhost:5173",      # React 개발 서버
+    "http://127.0.0.1:5173",      # 일부 브라우저에서 필요
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,        # 정확한 origin만 허용!
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # 라우터 등록
 app.include_router(voice_router.router)
